77f0014d3424d82888ae51a5a4d454230889be1b
Chen Yi-Hsuan <s3a932087@student.ncut.edu.tw>
2022-10-26 10:13:58 +0800

    update routes/web.php setting 修改Route接受參數

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,14 @@ Route::get('/', function() {    //回傳字串
     return 'welcome';
 });
 
-Route::get('r1', function() {
+Route::get('r1', function() {   //設定跳轉路由
     return redirect('r2');
 });
 Route::get('r2', function() {
     return view('welcome');
 });
+
+Route::get('hello/{name}', function($name) {    //修改route接受參數
+    return 'Hello, '.$name;
+});
+
