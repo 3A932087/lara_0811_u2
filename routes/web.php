45e6e711047a5957a6e4ef5c5f8e2bfbe8e8a094
Chen Yi-Hsuan <s3a932087@student.ncut.edu.tw>
2022-10-26 10:26:16 +0800

    update routes/web.php test route:list

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,7 @@ Route::get('r2', function() {
 Route::get('hello/{name}', function($name = 'Everybody') {    //修改參數成選擇性
     return 'Hello, '.$name;
 });
+
+Route::get('kk', function() {   //test route:list
+    return 'kk';
+});
